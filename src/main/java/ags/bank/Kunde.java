a01c7bc41a1c1c8853674adb9fc8534558bcd94b
Oleksandr Velychko <sanya.vel24@gmail.com>
2025-10-28 10:33:01 +0100

    filiale entities design reverted and updated to include a list

--- src/main/java/ags/bank/Kunde.java
+++ src/main/java/ags/bank/Kunde.java
@@ -6,15 +6,17 @@ public class Kunde extends Person {
     private int kundennummer;
     private Mitarbeiter betreuer;
     private ArrayList<Konto> kontos;
+    private Filiale filiale;
 
     public final int MAX_KONTEN_ANZAHL = 5;
 
     public Kunde(String vorname, String nachname, String strasse, String hausNummer, String email,
                  int kundennummer, Mitarbeiter betreuer, ArrayList<Konto> kontos, Filiale filiale) {
-        super(vorname, nachname, strasse, hausNummer, email, filiale);
+        super(vorname, nachname, strasse, hausNummer, email);
         this.kundennummer = kundennummer;
         this.betreuer = betreuer;
         this.kontos = kontos;
+        this.filiale = filiale;
     }
 
     // Getters & Setters
@@ -35,5 +37,11 @@ public class Kunde extends Person {
     }
     public void setBetreuer(Mitarbeiter betreuer) {
         this.betreuer = betreuer;
+    }
+    public Filiale getFiliale() {
+        return filiale;
+    }
+    public void setFiliale(Filiale filiale) {
+        this.filiale = filiale;
     }
 }
--- src/main/java/ags/bank/Mitarbeiter.java
+++ src/main/java/ags/bank/Mitarbeiter.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Mitarbeiter extends Person {
     private int mitarbeiterNummer;
     private ArrayList<Kunde> kundenListe;
+    private ArrayList<Filiale> filialeListe;
 
-    public Mitarbeiter(String vorname, String nachname, String strasse, String hausNummer, String email, Filiale filiale) {
-        super(vorname, nachname, strasse, hausNummer, email, filiale);
+    public Mitarbeiter(String vorname, String nachname, String strasse, String hausNummer, String email, ArrayList<Filiale> filialeListe) {
+        super(vorname, nachname, strasse, hausNummer, email);
+        this.filialeListe = filialeListe;
     }
 
     // Getters & Setters
@@ -22,5 +24,11 @@ public class Mitarbeiter extends Person {
     }
     public void setKundenListe(ArrayList<Kunde> kundenListe) {
         this.kundenListe = kundenListe;
+    }
+    public ArrayList<Filiale> getFilialeListe() {
+        return filialeListe;
+    }
+    public void setFilialeListe(ArrayList<Filiale> filialeListe) {
+        this.filialeListe = filialeListe;
     }
 }
